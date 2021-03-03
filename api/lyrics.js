62d50f2ba2d52172357c5b9dc0ec75f6cba84c25
Clément van Staen <clement.vanstaen@gmail.com>
2021-03-03 15:19:03 +0100

    init commit

---
+++ api/lyrics.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { Client } = require("pg");
+
+// Init Postgres
+const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: true })
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // This bypasses the SSL verification
+
+// Connect to Postgres 
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('statistic API:', 'Connected to postgres db!')
+    }
+})
+
+// GET all data from statistic
+router.get("/", async (req, res) => {
+    try {
+        const activities = await client.query('SELECT * FROM lyrics ORDER BY year ASC;');
+        res.status(201).json(activities.rows);
+    } catch (err) {
+        res.status(400).json({
+            error: `${err})`,
+        });
+    }
+});
+
+module.exports = router;
